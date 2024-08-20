ab4c04b826d60ade3b5c168a69629d984695c9a6
gk7734 <gamekiller7734@naver.com>
2024-08-20 18:05:40 +0900

    Navbar Complate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,17 +10,28 @@ const Navbar = () => {
         <>
             <nav className={`navContainer`}>
                 <ul className={`nav-grid`}>
-                    <li>
-                        <h1>Projects</h1>
-                    </li>
-                    <li>
-                        <h1>Abouts</h1>
-                    </li>
-                    <li>
-                        <h1>Contents</h1>
-                    </li>
-                    <li style={{ alignContent: "center", }}>
-                        <div onClick={() => setDark(!dark)}>
+                    <a href={'#projects'}>
+                        <li>
+                            <h1>Projects</h1>
+                        </li>
+                    </a>
+                    <a href={'#abouts'}>
+                        <li>
+                            <h1>Abouts</h1>
+                        </li>
+                    </a>
+                    <a href={'#blog'}>
+                        <li>
+                            <h1>Blog</h1>
+                        </li>
+                    </a>
+                    <a href={'#contents'}>
+                        <li>
+                            <h1>Contents</h1>
+                        </li>
+                    </a>
+                    <li style={{alignContent: "center",}}>
+                    <div onClick={() => setDark(!dark)} className={`circle-border`}>
                             {dark ? <MdSunny size={25}/> : <IoMoon size={25}/>}
                         </div>
                     </li>
